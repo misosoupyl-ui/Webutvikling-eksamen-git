92dcaf7d3ac624a477d3cc0a12cc694dc308d961
Soniia220 <soam004@student.kristiania.no>
2025-12-03 14:34:49 +0100

    endringer context og controller

---
+++ SportsWorldAPI/Controllers/AthleteController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameWorkCore;
+using SportsWorldAPI.Contexts;
+using SportsWorldAPI.Models
+
+namespace SportsWorldAPI.Controllers; 
+
+[ApiController]
+[Route("[controller]")]
+
+public class AthleteController() : ControllerBase
+{
+    
+}
