1d895d3a5a62d0b74f3e6a36d9cab74bcddd1ad1
misosoupyl-ui <misosoupyl@gmail.com>
2025-12-05 15:47:02 +0100

    fiksa på athlete service

--- SportsWorld/src/services/AthleteService.ts
+++ SportsWorld/src/services/AthleteService.ts
@@ -2,10 +2,64 @@ import type { IAthlete } from "../interfaces/IAthlete";
 
 import axios from "axios";
 
-const endpoint = "http://localhost:5105/athlete";
+const athleteEndpoint = "http://localhost:5105/athlete";
 
-//Anbefalt av Rolando i forelesning 20 1:27:20
+//Anbefalt av Rolando i forelesning 20 1:27:20, anbefalte både en try catch og returnere array
 const getAllAthletes = async (): Promise<IAthlete[]> => {
-  const response = await axios.get(endpoint);
-  return response.data;
+  try {
+    const response = await axios.get<IAthlete[]>(athleteEndpoint);
+    return response.data;
+  } catch (error) {
+    // Vi valgte å bruke console.error-> https://dev.to/johongirr/consolelog-consoleerror-consoleassert-and-more-1lf
+    console.error("Error:", error);
+    return [];
+  }
+};
+
+const getAthletesById = async (id: number): Promise<IAthlete | null> => {
+  try {
+    const response = await axios.get<IAthlete>(`${athleteEndpoint}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
+
+const postAthletes = async (newAthlete: IAthlete): Promise<IAthlete | null> => {
+  try {
+    const response = await axios.post<IAthlete>(athleteEndpoint, newAthlete);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
+
+const putAthletes = async (
+  editedAthlete: IAthlete
+): Promise<IAthlete | null> => {
+  try {
+    const response = await axios.put<IAthlete>(athleteEndpoint, editedAthlete);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
+
+const deleteAthletes = async (id: number): Promise<void> => {
+  try {
+    const response = await axios.delete<IAthlete>(`${athleteEndpoint}/${id}`);
+    console.log("Delete response:", response);
+  } catch (error) {
+    console.error("Error deleting athlete:", error);
+  }
+};
+export default {
+  getAllAthletes,
+  getAthletesById,
+  postAthletes,
+  putAthletes,
+  deleteAthletes,
 };
