dac4cc4abbb5b08681d62f20c4f66c074a91e45b
misosoupyl-ui <misosoupyl@gmail.com>
2025-12-05 16:57:36 +0100

    fikset finance service

---
+++ SportsWorld/src/services/FinanceService.ts
@@ -0,0 +1,41 @@
+import type { IFinance } from "../interfaces/IFinance";
+
+import axios from "axios";
+
+const financeEndpoint = "http://localhost:5105/finance";
+
+const getFinance = async (): Promise<IFinance | null> => {
+  try {
+    const response = await axios.get<IFinance>(financeEndpoint);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
+
+const addLoanMoney = async (loanAmount: number): Promise<IFinance | null> => {
+  try {
+    const response = await axios.put<IFinance>(financeEndpoint, { loanAmount });
+    return response.data;
+  } catch (error) {
+    console.error("Error adding loan", error);
+    return null;
+  }
+};
+
+const updateAfterPurchase = async (
+  purchaseAmount: number
+): Promise<IFinance | null> => {
+  try {
+    const response = await axios.put<IFinance>(financeEndpoint, {
+      purchaseAmount,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error updating finance", error);
+    return null;
+  }
+};
+
+export default { getFinance, addLoanMoney, updateAfterPurchase };
